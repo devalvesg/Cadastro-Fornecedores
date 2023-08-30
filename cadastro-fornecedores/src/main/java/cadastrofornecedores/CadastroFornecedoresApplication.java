7a1be4c4a8bd8b9dc98cfb84b0747df27d7b60aa
Gabriel Alves <gabrieldamasceno881@outlook.com>
2023-08-30 10:24:46 -0300

    Gerando o projeto

---
+++ cadastro-fornecedores/src/main/java/cadastrofornecedores/CadastroFornecedoresApplication.java
@@ -0,0 +1,13 @@
+package com.cautus.digital.cadastrofornecedores;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CadastroFornecedoresApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CadastroFornecedoresApplication.class, args);
+	}
+
+}
---
+++ cadastro-fornecedores/src/test/java/com/cautus/digital/cadastrofornecedores/CadastroFornecedoresApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cautus.digital.cadastrofornecedores;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadastroFornecedoresApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
